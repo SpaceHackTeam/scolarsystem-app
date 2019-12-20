c1d269451561cfbbd48570f4c1d7e088edfb7844
Dindonneau <bourdoncle.p@gmail.com>
2019-12-20 11:18:59 +0100

    deep ch**

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './assets/stylesheets/App.css';
-import ScolarSystem from './components/SolarSystem/SolarSystem';
+import React from "react";
+import "./assets/stylesheets/App.css";
+import Planet from "./components/Planet";
+import ScolarSystem from "./components/SolarSystem/SolarSystem";
 
 function App() {
   return (
     <div className="App">
       {/* <h1 className="Title">The S<span className="Title-c">c</span>olar System</h1> */}
       <ScolarSystem />
+      {/* <Planet /> */}
     </div>
   );
 }
--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,57 +1,119 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../assets/stylesheets/Planet.css";
 
-const Planet = input => (
-  <div className="planet-popup">
-    <img
-      className="planet-image"
-      src="https://www.numerama.com/content/uploads/2019/05/mars-espace-univers-planete-astronomie-systeme-solaire.jpg"
-      alt={input.name}
-    />
-    <div className="planet-infos">
-      <p className="planet-name">{input.name}</p>
-      <p className="planet-description">{input.description}</p>
-    </div>
-    <div className="planet-test">
-      <p className="planet-question">Quel est son rayon ?</p>
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-      <p className="planet-question">Température moyenne ?</p>
-      <input className="planet-qcm" type="radio" />
-      -65
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-      <p className="planet-question">
-        A quelle distance du soleil (minute-lumière) ?
-      </p>
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-      <p className="planet-question">Durée de révolution autour du soleil ?</p>
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-      <p className="planet-question">Combien de lunes gravitent autour ?</p>
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-      <input className="planet-qcm" type="radio" />
-      answer
-    </div>
-  </div>
-);
+const Planet = input => {
+  const [planetData, setPlanetData] = useState([]);
 
+  // const [planetQcm, setPlanetQcm] = useState({
+  //   radius: [],
+  //   mean_temp: [],
+  //   distance_light: [],
+  //   rotation_period: [],
+  //   qtyMoon: []
+  // })
+
+  useEffect(() => {
+    axios
+      .get("bdd.json")
+      .then(res => setPlanetData(res.data[6]))
+      .then(console.log(planetData));
+  }, []);
+
+  // const showAnswers = () =>
+  //   planetData.qcm &&
+  //   planetData.qcm.radius.map(element => (
+  //     <div>
+  //       <input className="planet-qcm" type="radio" /> {element}
+  //     </div>
+  //   ));
+
+  return (
+    <div className="planet-popup">
+      <img
+        className="planet-image"
+        src={planetData.img_url}
+        alt={planetData.name}
+      />
+      <div className="planet-infos">
+        <p className="planet-name">{planetData.name}</p>
+        <p className="planet-description">{planetData.description}</p>
+      </div>
+      <div className="planet-test">
+        <p className="planet-question">Quel est son rayon ?</p>
+        {/* {showAnswers()} */}
+
+        <div>
+          <input
+            type="radio"
+            id="rad1"
+            name="radius"
+            value={planetData.qcm && planetData.qcm.radius[0]}
+            checked
+          />
+          <label for="rad1">{planetData.qcm && planetData.qcm.radius[0]}</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="rad2"
+            name="radius"
+            value={planetData.qcm && planetData.qcm.radius[1]}
+            checked
+          />
+          <label for="rad2">{planetData.qcm && planetData.qcm.radius[1]}</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="rad3"
+            name="radius"
+            value={planetData.qcm && planetData.qcm.radius[2]}
+            checked
+          />
+          <label for="rad3">{planetData.qcm && planetData.qcm.radius[2]}</label>
+        </div>
+
+        <p className="planet-question">Température moyenne ?</p>
+
+        <div>
+          <input
+            type="radio"
+            id="mTemp1"
+            name="mean_temp"
+            value={planetData.qcm && planetData.qcm.mean_temp[0]}
+            checked
+          />
+          <label for="rad1">
+            {planetData.qcm && planetData.qcm.mean_temp[0]}
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="mTemp2"
+            name="mean_temp"
+            value={planetData.qcm && planetData.qcm.mean_temp[1]}
+            checked
+          />
+          <label for="rad2">
+            {planetData.qcm && planetData.qcm.mean_temp[1]}
+          </label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            id="mTemp3"
+            name="mean_temp"
+            value={planetData.qcm && planetData.qcm.mean_temp[2]}
+            checked
+          />
+          <label for="rad3">
+            {planetData.qcm && planetData.qcm.mean_temp[2]}
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Planet;
