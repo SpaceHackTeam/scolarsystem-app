4bca9d6a96cac16a472c971127061909c70da2cd
harshbones <davidrangeard@gmail.com>
2019-12-20 01:34:13 +0100

    added button

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,17 +5,24 @@ import './Intro.css';
 
 const decollageFusee = () => {
 	const sound = new Audio(require('../../assets/sound/fusee.mp3'));
+	document.getElementById('fuseeKiDecol').style.animation="slideIn";
 	return sound.play();
 };
 
 const Intro = () => {
-	decollageFusee();
 
 	return (
 		<div id="intro-bg">
 			<fuseeLourde />
 			<div className="container-fusee">
 				<img id="fuseeKiDecol" src={GifFusee} className="visible" />
+			</div>
+
+			<div class="button-container">
+			<button onClick={decollageFusee} class="redbutton">
+                <span class="launch">LAUNCH</span>
+            </button>
+            <div class="redbutton-shadow"></div>
 			</div>
 
 			<div id="intro-smoke" />
