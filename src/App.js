018da5ce6df37cee8f5498249099781d4af0936f
bat <batmon@laposte.net>
2019-12-20 07:37:30 +0100

    delete clock

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./assets/stylesheets/App.css";
 // import ScolarSystem from "./components/SolarSystem/SolarSystem";
 import Teslablackhole from "./components/blackhole/Teslablackhole";
-import FlipClock from "./components/clock/FlipClock";
 
 function App() {
   return (
     <div className="App">
       {/* <h1 className="Title">The S<span className="Title-c">c</span>olar System</h1> */}
       {/* <ScolarSystem /> */}
-      {/* <FlipClock /> */}
       <Teslablackhole />
     </div>
   );
