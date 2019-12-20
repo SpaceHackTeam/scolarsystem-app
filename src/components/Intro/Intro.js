0b71984e505f47b205eaa4c9983a7e82bbe18d8a
FuenCore <fuentes33160@gmail.com>
2019-12-20 01:59:28 +0100

    intro done

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -4,25 +4,26 @@ import '../../assets/sound/fusee.mp3';
 import './Intro.css';
 
 const decollageFusee = () => {
+	document.documentElement.requestFullscreen();
+	document.getElementById('fuseeKiDecol').style.animationPlayState = 'running';
+	document.getElementById('button-container').style.opacity = 0;
 	const sound = new Audio(require('../../assets/sound/fusee.mp3'));
-	document.getElementById('fuseeKiDecol').style.animation="slideIn";
 	return sound.play();
 };
 
 const Intro = () => {
-
 	return (
 		<div id="intro-bg">
 			<fuseeLourde />
 			<div className="container-fusee">
-				<img id="fuseeKiDecol" src={GifFusee} className="visible" />
+				<img id="fuseeKiDecol" src={GifFusee} />
 			</div>
 
-			<div class="button-container">
-			<button onClick={decollageFusee} class="redbutton">
-                <span class="launch">LAUNCH</span>
-            </button>
-            <div class="redbutton-shadow"></div>
+			<div id="button-container" class="button-container">
+				<button onClick={decollageFusee} class="redbutton">
+					<span class="launch">LAUNCH</span>
+				</button>
+				<div class="redbutton-shadow" />
 			</div>
 
 			<div id="intro-smoke" />
